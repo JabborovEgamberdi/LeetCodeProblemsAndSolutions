c55bd7fb794426db6a3f7ddf32524047d7a29b17
EgamberdiJabborov <jabborovegamberdi2002@gmail.com>
2023-07-18 22:58:31 +0500

    Refactored comments & added VCS mapping for Git
    Reorganized comments in Length_of_Last_Word_58.java to improve readability and understanding of the problem being solved.

--- src/Length_of_Last_Word_58.java
+++ src/Length_of_Last_Word_58.java
@@ -1,13 +1,22 @@
 /*
-*  Successfully finished with runtime 0 ms
-*  Time Complexity:
-*       first way: O (n + m);
-*                     n - length of input string;
-*                     m - length of trimmed string;
-*       second way: O (1)
-* */
+
+    Problem:
+  58. Length of Last Word
+  Given a string s consisting of words and spaces, return the length of the last word in the string.
+  A word is a maximal substring consisting of non-space characters only.
+
+    Result:
+  Successfully finished with runtime 0 ms
+  Time Complexity:
+       first way: O (n + m);
+                     n - length of input string;
+                     m - length of trimmed string;
+       second way: O (1)
+
+*/
 
 public class Length_of_Last_Word_58 {
+
     public static void main(String[] args) {
         String word = "   fly me   to   the moon  ";
         System.out.println("lengthOfLastWord(word) = " + lengthOfLastWordFirstWay(word));
@@ -30,16 +39,14 @@ public class Length_of_Last_Word_58 {
     public static int lengthOfLastWordSecondWay(String string) {
         int length = 0;
         int index = string.length() - 1;
-
         while (index >= 0 && string.charAt(index) == ' ') {
             index--;
         }
-
         while (index >= 0 && string.charAt(index) != ' ') {
             length++;
             index--;
         }
-
         return length;
     }
+
 }
