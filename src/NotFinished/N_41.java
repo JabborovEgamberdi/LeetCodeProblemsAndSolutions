f4e2c273692d591af1a117d5dddf17e42ca6ab7c
EgamberdiJabborov <jabborovegamberdi2002@gmail.com>
2024-03-28 09:09:02 +0500

    First hard problem started

--- src/NotFinished/N_41.java
+++ src/NotFinished/N_41.java
@@ -4,17 +4,18 @@ import java.util.Arrays;
 
 public class N_41 {
     public static void main(String[] args) {
-        int[] case1 = {7, 8, 9, 10, 11};
-        System.out.println(firstMissingPositive(case1));
-        int[] case2 = {3, 4, -1, 1};
-        System.out.println(firstMissingPositive(case2));
-        int[] case3 = {1, 2, 0};
-        System.out.println(firstMissingPositive(case3));
-        int[] case4 = {0, 2, 2, 1, 1};
-        System.out.println(firstMissingPositive(case4));
+        int[] nums = {1, 2, 0};
+        System.out.println(new N_41().firstMissingPositive(nums));
+        System.out.println(new N_41().firstMissingPositive(new int[]{3, 4, -1, 1}));
+        System.out.println(new N_41().firstMissingPositive(new int[]{7, 8, 9, 11, 12}));
+        System.out.println(new N_41().firstMissingPositive(new int[]{1, 2, 3, 4}));
+        System.out.println(new N_41().firstMissingPositive(new int[]{1, 1}));
+        System.out.println(new N_41().firstMissingPositive(new int[]{1}));
+        System.out.println(new N_41().firstMissingPositive(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        System.out.println(new N_41().firstMissingPositive(new int[]{2, 2, 1, 1, 0}));
     }
 
-    public static int firstMissingPositive(int[] nums) {
+    public int firstMissingPositive(int[] nums) {
         Arrays.sort(nums);
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -27,10 +28,8 @@ public class N_41 {
         boolean[] arr = new boolean[nums.length];
         for (int i = index; i < nums.length; i++) {
             if (nums[i] != num) return num;
-            if (!arr[nums[i]]) {
-                num++;
-            }
-            arr[nums[i]] = true;
+            if (!arr[nums[i]]) num++;
+            else arr[nums[i]] = true;
         }
         return num;
     }
