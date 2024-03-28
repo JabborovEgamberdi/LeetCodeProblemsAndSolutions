bf9aef4531222498c31c00698bf6277cbe905f60
EgamberdiJabborov <jabborovegamberdi2002@gmail.com>
2024-03-28 23:30:44 +0500

    First hard question is finished with 0 ms runtime

--- src/NotFinished/N_41.java
+++ src/NotFinished/N_41.java
@@ -1,38 +1,60 @@
 package NotFinished;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
+
+/*
+* Level: 🔴🔴🔴
+* Status: DONE
+* Time Complexity: O(n)
+* Space Complexity: O(1)
+* Runtime: 0 ms Beats 100.00% of users with Java
+* */
 public class N_41 {
     public static void main(String[] args) {
         int[] case1 = {7, 8, 9, 10, 11};
         System.out.println(firstMissingPositive(case1));
         int[] case2 = {3, 4, -1, 1};
-        System.out.println(firstMissingPositive(case2));
+        System.out.println(firstMissingPositive2(case2));
         int[] case3 = {1, 2, 0};
-        System.out.println(firstMissingPositive(case3));
+        System.out.println(firstMissingPositive2(case3));
         int[] case4 = {0, 2, 2, 1, 1};
-        System.out.println(firstMissingPositive(case4));
+        System.out.println(firstMissingPositive2(case4));
     }
 
+    // it works but TLE
     public static int firstMissingPositive(int[] nums) {
         Arrays.sort(nums);
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                index = i;
-                break;
+        List<Integer> list = new LinkedList<>();
+        for (int j : nums) {
+            if (j > 0) {
+                if (!list.contains(j)) {
+                    list.add(j);
+                }
             }
         }
         int num = 1;
-        boolean[] arr = new boolean[nums.length];
-        for (int i = index; i < nums.length; i++) {
-            if (nums[i] != num) return num;
-            if (!arr[nums[i]]) {
-                num++;
-            }
-            arr[nums[i]] = true;
+        for (Integer integer : list) {
+            if (integer != num) return num;
+            num++;
         }
         return num;
+    }
+
+    // 0 ms --> 100 ms
+    public static int firstMissingPositive2(int[] nums) {
+        boolean[] arr = new boolean[nums.length + 1];
+        for (int num : nums) {
+            if (num > 0 && num <= nums.length) {
+                arr[num] = true;
+            }
+        }
+        for (int i = 1; i < arr.length; i++) {
+            if (!arr[i]) return i;
+        }
+        return -1;
     }
 
 }
