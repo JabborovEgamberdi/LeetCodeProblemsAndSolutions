b9724fe87e97b61fad179f9994297963a3838e98
EgamberdiJabborov <jabborovegamberdi2002@gmail.com>
2024-04-14 00:12:52 +0500

    N_242 problem is successfully solved

---
+++ src/N_242.java
@@ -0,0 +1,43 @@
+/*
+* Level: 🟢🟢🟢
+* Status: DONE
+* Time Complexity: O(n)
+* Space Complexity: O(1)
+* Runtime: 2 ms Beats 98.34% of users with Java
+* */
+
+/*
+* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+*
+* Example 1:
+* Input: s = "anagram", t = "nagaram"
+* Output: true
+*
+* Example 2:
+* Input: s = "rat", t = "car"
+* Output: false
+*
+* Constraints:
+* 1 <= s.length, t.length <= 5 * 104
+* s and t consist of lowercase English letters.
+* */
+public class N_242 {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        int[] arr = new int[26];
+        for (char c : s.toCharArray()) {
+            arr[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            arr[c - 'a']--;
+        }
+        for (int i : arr) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+
+}
